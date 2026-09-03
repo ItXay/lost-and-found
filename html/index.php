7c76ea47dbeae8f82a10bf4c6ec2da79091e9510
NimbusAdmin69 <ukrainianteenager99@gmail.com>
2026-09-03 13:12:43 +0200

    Correct HTML structure and typos in index.php
    Fixed typos in HTML structure and added missing elements.

--- html/index.php
+++ html/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	privet
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -27,6 +28,7 @@
 			Skapa annons 
 		</a>
 	</hrader>
+	
 
 
 	<main>
