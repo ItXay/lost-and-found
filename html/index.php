0cd294c5bdb2cd7f21b429db1942d475e66511b4
ItXay <138992185+ItXay@users.noreply.github.com>
2026-09-03 12:50:33 +0200

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Lost & Found</title>
+
+	<link rel="stylesheet" href="css/syle.css">
+</head>
+
+<body>
+	
+	<hrader class="novbar">
+		<div class="logo">
+			Lost & Found
+		</div>
+
+
+		<nav class="nav-links">
+			<a href="index.php">Hem</a>
+			<a href="ads.php">Annonser</a>
+			<a href="about.php">Om oss</a>
+		</nav>
+
+		<a class="create-button" href="create-ad.php">
+			Skapa annons 
+		</a>
+	</hrader>
+
+
+	<main>
+
+		<section class="hero">
+
+			<div class="hero-content">
+
+				<p class="hero-small-text">
+					Tillsammans gör vi det lättare att hitta rätt.
+				</p>
+
+				<h1>
+					Hitta borttappade saker enklare
+				</h1>
+
+				<p>
+					Sök bland förlorande saker enklare
+				</p>
+
+				<form class="search-box">
+					<input 
+					type="text" 
+					placeholder="Sök efter sak..."
+					>
+
+					<button type="sumbit">
+						Sök
+					</button>
+				</form>
+
+
+
+
+
+
+
+
+</body>
